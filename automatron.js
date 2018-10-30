5fe150584b50b940fc3b9d141aad13a8bca8103f
Thai Pangsakulyanont <dtinth@spacet.me>
2018-10-30 21:49:34 +0700

    Updating automatron.js

--- automatron.js
+++ automatron.js
@@ -1,6 +1,40 @@
+const express = require('express')
+const Webtask = require('webtask-tools')
+const bodyParser = require('body-parser')
+const middleware = require('@line/bot-sdk').middleware
+const Client = require('@line/bot-sdk').Client
+const app = express()
+
 /**
- * @param context {WebtaskContext}
+ * @param {WebtaskContext} context
+ * @param {import('@line/bot-sdk').WebhookEvent[]} events
+ * @param {import('@line/bot-sdk').Client} client
  */
-module.exports = function(context, cb) {
-  cb(null, { hello: context.query.name || 'Anonymous' });
-};
+async function handleWebhook(context, events, client) {
+  return 1
+}
+
+app.post('/webhook', (req, res, next) => {
+  /** @type {WebtaskContext} */
+  const lineConfig = getLineConfig(req)
+  middleware(lineConfig)(req, res, async err => {
+    if (err) return next(err)
+    try {
+      const client = new Client(config)
+      const data = handleWebhook(ctx, req.body.events, client)
+      res.json({ data })
+    } catch (e) {
+      return next(err)
+    }
+  })
+})
+
+function getLineConfig(req) {
+  const ctx = req.webtaskContext
+  return {
+    channelAccessToken: ctx.secrets.LINE_CHANNEL_ACCESS_TOKEN,
+    channelSecret: ctx.LINE_CHANNEL_SECRET
+  }
+}
+
+module.exports = Webtask.fromExpress(app)

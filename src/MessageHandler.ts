f464715d2870ee4e0884143d731d642027a0a054
dtinth on Cloud9 <dtinth@spacet.me>
2020-12-20 17:48:55 +0000

    Add self variable for adding extra prelude

--- src/MessageHandler.ts
+++ src/MessageHandler.ts
@@ -86,14 +86,16 @@ export async function handleTextMessage(
       .replace(/^\(function/, '(async function')
     console.log('Code compilation result', code)
     const runner = new Function(
-      ...['prelude', 'code', 'context', 'state'],
+      ...['prelude', 'self', 'code', 'context', 'state'],
       'with (prelude) { with (state) { return [ eval(code), state ] } }'
     )
     // TODO: Load storage to `prevStateSnapshot`
     const prevStateSnapshot = '{}'
     const prevState = JSON.parse(prevStateSnapshot)
+    const self = {}
     const [value, nextState] = runner(
       require('prelude-ls'),
+      self,
       code,
       context,
       prevState

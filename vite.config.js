f34f0e768b8bee73bc13bd2697c89ffa3f11b716
dtinth on MBP M1 <dtinth@spacet.me>
2021-09-21 19:58:38 +0700

    Basic sign in stuff

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [
+    svelte(),
+
+    VitePWA({
+      manifest: {
+        short_name: 'automatron',
+        name: 'automatron',
+        start_url: '/',
+        icons: [
+          {
+            src: '/automatron.png',
+            type: 'image/png',
+          },
+        ],
+        display: 'standalone',
+        theme_color: '#090807',
+        background_color: '#353433',
+      },
+    }),
+  ],
 })

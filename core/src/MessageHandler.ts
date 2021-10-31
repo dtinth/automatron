ebf0630d2cb41bd80da1a22d84079ed158954bba
Thai Pangsakulyanont <dtinth@spacet.me>
2021-10-31 09:28:56 +0000

    Move the main project into Rush monorepo

--- core/src/MessageHandler.ts
+++ core/src/MessageHandler.ts
@@ -62,17 +62,19 @@ export async function handleTextMessage(
       : +m[1]
     const conversionRate = m[2] ? 0.302909 : 1
     const amount = (enteredAmount * conversionRate).toFixed(2)
-    const category = ({
-      t: 'transportation',
-      f: 'food',
-      g: 'game',
-      h: 'health',
-      m: 'miscellaneous',
-      o: 'occasion',
-      l: 'lodging',
-    } as {
-      [k: string]: string
-    })[m[3].toLowerCase()]
+    const category = (
+      {
+        t: 'transportation',
+        f: 'food',
+        g: 'game',
+        h: 'health',
+        m: 'miscellaneous',
+        o: 'occasion',
+        l: 'lodging',
+      } as {
+        [k: string]: string
+      }
+    )[m[3].toLowerCase()]
     const remarks = m[2] ? `${m[1]} JPY` : ''
     return await recordExpense(context, amount, category, remarks)
   } else if ((match = message.match(/^([ivxlcdm]+)$/i))) {
@@ -90,7 +92,7 @@ export async function handleTextMessage(
       ...extraMessages,
     ]
   }
-  return 'unrecognized message...'
+  return '(unrecognized message...)'
 }
 
 export async function handleImage(

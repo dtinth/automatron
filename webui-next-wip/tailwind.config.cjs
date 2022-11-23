2c8d6369c6152bf27feb144976a7926445097b8c
dtinth on MBP M1 <dtinth@spacet.me>
2022-11-23 21:54:57 +0700

    Add webui-next-wip

---
+++ webui-next-wip/tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        '#e9e8e7': '#e9e8e7',
+        '#8b8685': '#8b8685',
+        '#656463': '#656463',
+        '#454443': '#454443',
+        '#353433': '#353433',
+        '#252423': '#252423',
+        '#090807': '#090807',
+        '#bbeeff': '#bbeeff',
+        '#d7fc70': '#d7fc70',
+        '#ffffbb': '#ffffbb',
+      },
+    },
+  },
+  plugins: [],
+}

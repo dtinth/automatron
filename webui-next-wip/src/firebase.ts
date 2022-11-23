69e75a0181422afec88d8613e7409af35edb6965
dtinth on MBP M1 <dtinth@spacet.me>
2022-11-23 22:41:11 +0700

    Add Firebase

---
+++ webui-next-wip/src/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDp9XHcCy-sz1m65cr_ei5KaXHmRaUspTI',
+  authDomain: 'dtinth-automatron.firebaseapp.com',
+  projectId: 'dtinth-automatron',
+  storageBucket: 'dtinth-automatron.appspot.com',
+  messagingSenderId: '347735770628',
+  appId: '1:347735770628:web:8fce6a02b34c2d17c2d751',
+}
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig)

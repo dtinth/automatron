aabd1f94dcebb66e7f9d8c522c8ed418557f3a16
Thai Pangsakulyanont <dtinth@spacet.me>
2023-01-04 15:02:58 +0000

    Add context

--- core/src/LanguageModelAssistant.ts
+++ core/src/LanguageModelAssistant.ts
@@ -13,19 +13,39 @@ export const LanguageModelAssistantMessageHandler: TextMessageHandler = (
         context,
         'c0wgjM3RJp/V40lLhcHbtjUDBvlT/NlI.yF9iWwImsHrUOiZkD6UMZdGyjLd3yCJm7WMkN6dxerzXlxCK4U6bSzNFHwyvUjPDop4+gLCs5Qa9Pcxwii3DvSVjjE+7'
       )
-      const prompt = text.slice(3)
+      const nowIct = new Date(Date.now() + 7 * 3600e3)
+      const day = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+      ][nowIct.getDay()]
+      const prompt =
+        // https://beta.openai.com/examples/default-chat
+        'The following is a conversation with an AI assistant, automatron. ' +
+        'The assistant is helpful, creative, clever, funny, and very friendly. ' +
+        'It always responds in English, regardless of the language of the input. ' +
+        '\n' +
+        'Current time: ' +
+        nowIct.toISOString().replace('Z', '+07:00') +
+        ' (Asia/Bangkok).\n' +
+        'Today is ' +
+        day +
+        '.' +
+        '\n' +
+        'Human: Hello, who are you?\n' +
+        "automatron: Hi there! I'm automatron, your friendly AI assistant! Let me know what I can do for you!\n" +
+        'Human: ' +
+        text.slice(3) +
+        '\n' +
+        'automatron:'
+
       const payload = {
         model: 'text-davinci-003',
-        prompt:
-          // https://beta.openai.com/examples/default-chat
-          'The following is a conversation with an AI assistant, automatron. ' +
-          'The assistant is helpful, creative, clever, and very friendly.\n\n' +
-          'Human: Hello, who are you?\n' +
-          "automatron: Hi there! I'm automatron, your friendly AI assistant! Let me know what I can do for you!\n" +
-          'Human: ' +
-          prompt +
-          '\n' +
-          'automatron:',
+        prompt,
         temperature: 0.9,
         max_tokens: 160,
         top_p: 1,

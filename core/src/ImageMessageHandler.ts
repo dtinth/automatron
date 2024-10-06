8fa4f028c58c4e9618464ba6f6cc85ea71563ed1
Thai Pangsakulyanont <dtinth@spacet.me>
2024-10-06 05:36:40 +0000

    improve image handling

--- core/src/ImageMessageHandler.ts
+++ core/src/ImageMessageHandler.ts
@@ -1,52 +1,63 @@
-import { ref } from './PersistentState'
-import { TextMessage, TextMessageHandler } from './types'
 import vision from '@google-cloud/vision'
-import { getBlob } from './TemporaryBlobStorage'
+import { ref } from './PersistentState'
+import { getBlob, getBlobUrl } from './TemporaryBlobStorage'
+import { TextMessage, TextMessageHandler } from './types'
 
 export const ImageMessageHandler: TextMessageHandler = (text, context) => {
   if (text.startsWith('image:')) {
     return async () => {
       const blobName = text.slice(6)
-      const buffer = await getBlob(blobName)
       await ref(context, 'latestImage').set(blobName)
-      const imageAnnotator = new vision.ImageAnnotatorClient()
-      const results = await imageAnnotator.documentTextDetection(buffer)
-      const fullTextAnnotation = results[0].fullTextAnnotation
-      const blocks: string[] = []
-      for (const page of fullTextAnnotation.pages) {
-        blocks.push(
-          ...page.blocks.map((block) => {
-            return block.paragraphs
-              .map((p) =>
-                p.words
-                  .map((w) => w.symbols.map((s) => s.text).join(''))
-                  .join(' ')
-              )
-              .join('\n\n')
-          })
-        )
-      }
-      const blocksToResponses = (blocks: string[]) => {
-        if (blocks.length <= 4) return blocks
-        let processedIndex = 0
-        const outBlocks = []
-        for (let i = 0; i < 4; i++) {
-          const targetIndex = Math.ceil(((i + 1) * blocks.length) / 4)
-          outBlocks.push(
-            blocks
-              .slice(processedIndex, targetIndex)
-              .map((x) => `・ ${x}`)
-              .join('\n')
-          )
-          processedIndex = targetIndex
-        }
-        return outBlocks
-      }
-      const responses = blocksToResponses(blocks)
-      return [
-        { type: 'text', text: blobName } as TextMessage,
-        ...responses.map((r): TextMessage => ({ type: 'text', text: r })),
-      ]
+      return [{ type: 'text', text: blobName }]
     }
   }
+  if (text === 'annotate') {
+    return async () => {
+      const blobName = await ref(context, 'latestImage').get()
+      return await annotateImage(blobName)
+    }
+  }
+  if (text === 'image url') {
+    return async () => {
+      const blobName = await ref(context, 'latestImage').get()
+      return await getBlobUrl(blobName)
+    }
+  }
+}
+
+async function annotateImage(blobName: string) {
+  const buffer = await getBlob(blobName)
+  const imageAnnotator = new vision.ImageAnnotatorClient()
+  const results = await imageAnnotator.documentTextDetection(buffer)
+  const fullTextAnnotation = results[0].fullTextAnnotation
+  const blocks: string[] = []
+  for (const page of fullTextAnnotation.pages) {
+    blocks.push(
+      ...page.blocks.map((block) => {
+        return block.paragraphs
+          .map((p) =>
+            p.words.map((w) => w.symbols.map((s) => s.text).join('')).join(' ')
+          )
+          .join('\n\n')
+      })
+    )
+  }
+  const blocksToResponses = (blocks: string[]) => {
+    if (blocks.length <= 4) return blocks
+    let processedIndex = 0
+    const outBlocks = []
+    for (let i = 0; i < 4; i++) {
+      const targetIndex = Math.ceil(((i + 1) * blocks.length) / 4)
+      outBlocks.push(
+        blocks
+          .slice(processedIndex, targetIndex)
+          .map((x) => `・ ${x}`)
+          .join('\n')
+      )
+      processedIndex = targetIndex
+    }
+    return outBlocks
+  }
+  const responses = blocksToResponses(blocks)
+  return [...responses.map((r): TextMessage => ({ type: 'text', text: r }))]
 }

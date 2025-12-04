a5ffa748f4c20befeccfdf362e861e2b3a478e5d
dtinth on MBP M1 <dtinth@spacet.me>
2025-12-05 00:58:15 +0700

    prepare for do app service

---
+++ core/src/server.ts
@@ -0,0 +1,39 @@
+import { Storage } from '@google-cloud/storage'
+import { start } from '@google-cloud/trace-agent'
+import express from 'express'
+
+const bot = require('./bot')
+const tracer = start()
+
+const app = express()
+const PORT = process.env.PORT || 28364
+
+const storage = new Storage()
+const [, bucket, file] = process.env.AUTOMATRON_ENV_GS_URI!.match(
+  /^gs:\/\/([^\/]+)\/(.+)$/
+)!
+
+function loadEnv() {
+  return storage
+    .bucket(bucket)
+    .file(file)
+    .download()
+    .then(([data]) => {
+      const env = require('dotenv').parse(data)
+      return env
+    })
+}
+let envPromise = loadEnv()
+
+app.use(async (req, res, next) => {
+  try {
+    req.tracer = tracer
+    req.env = await envPromise
+    bot(req, res)
+  } catch (err) {
+    next(err)
+  }
+})
+app.listen(PORT, () => {
+  console.log(`Automatron server is running on port ${PORT}`)
+})
